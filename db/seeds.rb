f4bebc2a5775b07993e4237c9b3a9e129ac38f5b
apalzab <apalzab@gmail.com>
2015-04-11 10:38:23 +0200

    Some seed data added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Movie
+movie1 = Movie.create(title: 'The Lord of the rings', plot: 'sbjhdsjhjdsjdsl')
+movie2 = Movie.create(title: 'Batman', plot: 'sbjhdsjhjdsjdsl')
+movie3 = Movie.create(title: 'Whatever works', plot: 'sbjhdsjhjdsjdsl')
+
+# PurchaseOption
+p1 = PurchaseOption.create(quality: 'hd', price: 10)
+p2 = PurchaseOption.create(quality: 'sd', price: 5)
+
+p3 = PurchaseOption.create(quality: 'hd', price: 20)
+p4 = PurchaseOption.create(quality: 'sd', price: 10)
+
+p5 = PurchaseOption.create(quality: 'hd', price: 30)
+p6 = PurchaseOption.create(quality: 'sd', price: 20)
+
+# MoviePurchaseOption
+mpo1 = MoviePurchaseOption.create(movie_id: movie1.id, purchase_option_id: p1)
+mpo2 = MoviePurchaseOption.create(movie_id: movie1.id, purchase_option_id: p2)
+
+mpo3 = MoviePurchaseOption.create(movie_id: movie2.id, purchase_option_id: p3)
+mpo4 = MoviePurchaseOption.create(movie_id: movie2.id, purchase_option_id: p4)
+
+mpo5 = MoviePurchaseOption.create(movie_id: movie3.id, purchase_option_id: p5)
+mpo6 = MoviePurchaseOption.create(movie_id: movie3.id, purchase_option_id: p6)
+
+# User
+user1 = User.create(nickname: 'bender')
+
+# Purchase
+purchase = Purchase.create(movie_purchase_option_id: mpo1.id, user_id: user1.id)
+purchase = Purchase.create(movie_purchase_option_id: mpo4.id, user_id: user1.id)
+purchase = Purchase.create(movie_purchase_option_id: mpo5.id, user_id: user1.id)
