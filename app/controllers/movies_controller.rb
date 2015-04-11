b36c01d974698eb531e5891bfda4197cc83ea975
apalzab <apalzab@gmail.com>
2015-04-11 17:12:30 +0200

    infinite_scrolling done with js

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.paginate(page: params[:page], per_page: 15).order('created_at DESC')
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
 end
