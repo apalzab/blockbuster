ce771f7a0876b489be084cf2f860475a5c148f3a
apalzab <apalzab@gmail.com>
2015-04-11 10:23:26 +0200

    add_index to purchases

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410193520) do
+ActiveRecord::Schema.define(version: 20150411081846) do
 
   create_table "movie_purchase_options", force: :cascade do |t|
     t.integer  "movie_id"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20150410193520) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
+
+  add_index "purchases", ["movie_purchase_option_id"], name: "index_purchases_on_movie_purchase_option_id"
+  add_index "purchases", ["user_id"], name: "index_purchases_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
