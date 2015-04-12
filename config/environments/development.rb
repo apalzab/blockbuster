bc1451227ee0f7bfea7e12b63aa1342715b0ff36
apalzab <apalzab@gmail.com>
2015-04-12 19:22:55 +0200

    caching removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_controller.perform_caching = true
+  # config.action_controller.perform_caching = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
